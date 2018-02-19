4bd015194e231f431453ce43df8406dcfbe5284e
Damian <damiankwasny95@gmail.com>
2018-02-19 19:25:48 +0100

    Hash function and hash table implementation adn brief test

--- StrukturyDanychIAlgorytmy/scripts/hashingTables/hashingTablesModule.py
+++ StrukturyDanychIAlgorytmy/scripts/hashingTables/hashingTablesModule.py
@@ -18,6 +18,12 @@ class myHashItem:
         self.value = value
 
 class myHashTable:
+    '''
+    Hashtable is used when we know what we are searching for (i.e. known key).
+    We can use this key as an index, and immediately return a value. Problem
+    arises when the hastable count approaches its size, because it slows down
+    both get and put operations
+    '''
     def __init__(self):
         self.size = 256       #maximum number of slots
         self.slots = [None for i in range(self.size)]   #stores elements in list
@@ -28,12 +34,28 @@ class myHashTable:
 
     def put(self, key, value):
         item = myHashItem(key, value)
-        h = _hash(key)
-        while self.slots[h] is not None:
-            if self.slots[h].key is key:
+        h = self._hash(key)                  #calculating the hash of they key
+        while self.slots[h] is not None:#if slot with index h is not empty
+            if self.slots[h].key is key:#if it is the same as they key, do nothin
                 break
             else:
-                h = (h+1)%self.size
-        if self.slots[h] is None:
-            self.count += 1
-        self.slots[h] = item
+                h = (h+1)%self.size     #else, check next hash index
+        if self.slots[h] is None:       #if slot with hash index h is free/empty
+            self.count += 1             #increment count of table
+        self.slots[h] = item            #Put an item at thios hash index
+
+    def get(self, key):
+        '''
+        To get the value associated with the key 'key' we peroform opposite
+        of put procedure
+        '''
+        h = self._hash(key)                  #again, get the hash of the key
+        while self.slots[h] is not None:#check table element at ind h
+            if self.slots[h].key is key:#if they keys match, return value
+                return self.slots[h].value
+            else:
+                h = (h+1)%self.size     #else, check next index
+    def __setitem__(self, key, value):
+        self.put(key, value)
+    def __getitem__(self, key):
+        self.get(key)
--- StrukturyDanychIAlgorytmy/scripts/hashingTables/hashingTablesTestScript.py
+++ StrukturyDanychIAlgorytmy/scripts/hashingTables/hashingTablesTestScript.py
@@ -6,3 +6,17 @@ from hashingTablesModule import *
 print('myHash function test')
 for item in ('hello world', 'world hello', 'gello xorld'):
     print('{string}: {key}'.format(string = item, key = myHash(item)))
+
+# myHashTable test:
+testHashTable = myHashTable()
+
+#1. puting elements:
+testHashTable.put('ongaku', '5')
+testHashTable.put('anime', '4')
+testHashTable.put('hon', '3')
+testHashTable.put('ski', '2')
+testHashTable.put('tabemono', '1')
+
+#2. getting and printing elements
+for key in ('ongaku', 'anime','ski','hon','tabemono'):
+    print(testHashTable.get(key)) # 5 4 2 3 1
